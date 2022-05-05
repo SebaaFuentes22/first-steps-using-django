649401ac63f26962c74fc1b5faddbb4821a122d5
Sebastian <ss.programacion@gmail.com>
2022-05-04 23:55:24 -0400

    comienzo1

---
+++ personalProject/petclub/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class HelloWorld(APIView):
+    def get(self, request): # verbo de la peticion como un metodo
+        # logica asociada al endpoint
+        return Response(data="Hello, World !", status=200) # respuesta del servicio
+
+
+# Create your views here.
